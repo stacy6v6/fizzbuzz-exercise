172da749b77db08b9f22d27008fab0b4dcbed82a
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2023-01-24 09:38:47 +0800

    additional unit tests

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,7 @@
 const fizzbuzz = (integer) => {
+  if (!(Number.isInteger(integer)) || integer <= 0) {
+    throw new TypeError('Invalid input!')
+  }
   if (integer % 3 === 0 && integer % 5 === 0) {
     return 'fizzbuzz'
   } if (integer % 3 === 0) {
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,8 +1,12 @@
 import { expect, test } from '@jest/globals'
 import fizzbuzz from './fizzbuzz.js'
 
-test('Returns empty string for invalid input', () => {
-  expect(fizzbuzz('hello')).toBe('')
+test('Throws for invalid input (string)', () => {
+  expect(() => fizzbuzz('hello')).toThrow()
+})
+test('Throws for invalid input (non-positive integer)', () => {
+  expect(() => fizzbuzz(-1)).toThrow('Invalid input!')
+  expect(() => fizzbuzz(0)).toThrow('Invalid input!')
 })
 test('Returns empty string for number not divisible by 3 and 5', () => {
   expect(fizzbuzz(2)).toBe('')
