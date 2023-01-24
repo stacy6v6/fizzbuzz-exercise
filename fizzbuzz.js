c8f6dad7c0e83eaea5403585d5d907597274415f
Stacy Marie Pelito <stacymarie.pelito@stacymarie.pelito-C52FJWWCQJ>
2023-01-24 10:08:25 +0800

    add aditional testing for different errors

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,9 @@
 const fizzbuzz = (integer) => {
-  if (!(Number.isInteger(integer)) || integer <= 0) {
-    throw new TypeError('Invalid input!')
+  if (!(Number.isInteger(integer))) {
+    throw new TypeError('Invalid input type! Must be a positive integer.')
+  }
+  if (integer <= 0) {
+    throw new RangeError('Invalid integer range! Must be a positive integer.')
   }
   if (integer % 3 === 0 && integer % 5 === 0) {
     return 'fizzbuzz'
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,22 +1,24 @@
-import { expect, test } from '@jest/globals'
+import { describe, expect, test } from '@jest/globals'
 import fizzbuzz from './fizzbuzz.js'
 
-test('Throws for invalid input (string)', () => {
-  expect(() => fizzbuzz('hello')).toThrow()
-})
-test('Throws for invalid input (non-positive integer)', () => {
-  expect(() => fizzbuzz(-1)).toThrow('Invalid input!')
-  expect(() => fizzbuzz(0)).toThrow('Invalid input!')
-})
-test('Returns empty string for number not divisible by 3 and 5', () => {
-  expect(fizzbuzz(2)).toBe('')
-})
-test('Returns fizzbuzz for number divisible by 3 and 5', () => {
-  expect(fizzbuzz(15)).toBe('fizzbuzz')
-})
-test('Returns fizz for number divisible by 3', () => {
-  expect(fizzbuzz(33)).toBe('fizz')
-})
-test('Returns buzz for number divisible by 5', () => {
-  expect(fizzbuzz(50)).toBe('buzz')
+describe('Unit testing for fizzbuzz', () => {
+  test('Throws for invalid input (string)', () => {
+    expect(() => fizzbuzz('hello')).toThrow('Invalid input type! Must be a positive integer.')
+  })
+  test('Throws for invalid input (non-positive integer)', () => {
+    expect(() => fizzbuzz(-1)).toThrow('Invalid integer range! Must be a positive integer.')
+    expect(() => fizzbuzz(0)).toThrow('Invalid integer range! Must be a positive integer.')
+  })
+  test('Returns empty string for number not divisible by 3 and 5', () => {
+    expect(fizzbuzz(2)).toBe('')
+  })
+  test('Returns fizzbuzz for number divisible by 3 and 5', () => {
+    expect(fizzbuzz(15)).toBe('fizzbuzz')
+  })
+  test('Returns fizz for number divisible by 3', () => {
+    expect(fizzbuzz(33)).toBe('fizz')
+  })
+  test('Returns buzz for number divisible by 5', () => {
+    expect(fizzbuzz(50)).toBe('buzz')
+  })
 })
